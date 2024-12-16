39ed5dbae0388ff8bcbccc724536a8f44176a706
muhammadega21 <dermawane988@gmail.com>
2024-12-16 22:44:45 +0700

    update 5 files

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,6 +4,8 @@ import "aos/dist/aos.css";
 import BackToTop from "../components/Elements/BackToTop";
 import "./index.css";
 import "./about.css";
+import { WrapperAbout } from "../components/Fragments/Wrapper";
+import Footer from "./../components/Layouts/Footer";
 
 function About() {
   useEffect(() => {
@@ -31,12 +33,12 @@ function About() {
             <h2 className="leading-[45px] md:leading-[60px] my-4 mb-5">
               Professional Web Programming
             </h2>
-            <p className="mt-4">
+            <p>
               Ability to put themselves in the merchant's shoes. It is meant to
               partner on the long run, and work as an extension of the
               merchant's team.
             </p>
-            <p className="mt-4">
+            <p>
               A digital agency is a business you hire to outsource your digital
               marketing efforts, instead of handling in-house. They can provide
               your business with a variety of digital solutions to promote your
@@ -44,7 +46,55 @@ function About() {
             </p>
           </div>
         </div>
+        <div className="my-4">
+          <WrapperAbout />
+        </div>
+        <div className="about-rell w-full md:w-1/2 mx-auto">
+          <h3 className="leading-[45px] md:leading-[50px] my-4 mb-5">
+            Video Perkenalan Tentang Saya
+          </h3>
+          <p>
+            Design, business understanding, ability to put themselves in the
+            merchant's shoes meant to partner.
+          </p>
+          <video className="w-full rounded my-6" controls>
+            <source src={"videos/intro.mp4"} type={"video/mp4"} />
+          </video>
+          <p>
+            Ability to put themselves in the merchant's shoes. It is meant to
+            partner on the long run, and work as an extension of the merchant's
+            team
+          </p>
+          <p>
+            A digital agency is a business you hire to outsource your digital
+            marketing efforts, instead of handling in-house. They can provide
+            your business with a variety of digital solutions to promote your
+            product or service online and help you.
+          </p>
+        </div>
+        <div className="bg-[#F3D1BF] h-[1px] w-full my-12"></div>
+        <div className="quote w-full md:w-1/2 mx-auto">
+          <h5>FOUNDER WORDS</h5>
+          <div className="relative">
+            <h3 className="leading-[40px] md:leading-[50px] my-5 relative z-10">
+              Digital agency is a business you hire to outsource your digital
+              marketing efforts
+            </h3>
+            <img
+              src="icons/quote.svg"
+              alt="quote"
+              className="absolute -top-10 right-0"
+            />
+          </div>
+          <p className="!text-[#391400]">Gabriel Pires</p>
+          <span className="text-[#391400A3] text-sm">Founder</span>
+        </div>
+        <div className="bg-[#F3D1BF] h-[1px] w-full my-12"></div>
+        <div className="text-center">Organizations...</div>
       </section>
+      <div className="dark py-14">
+        <Footer />
+      </div>
     </div>
   );
 }
